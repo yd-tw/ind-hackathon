f75f88b56c89c9fee3bc53bc2ea0e49eb78d07ae
yd-tw <yd960528@gmail.com>
2025-09-21 17:54:38 +0800

    update package

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const res = await fetch(
+      "https://tronclass.ntou.edu.tw/api/todos?no-intercept=true",
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "X-SESSION-ID": process.env.Session!,
+        },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error(`API 回應錯誤: ${res.status}`);
+    }
+
+    const data = await res.json();
+    return NextResponse.json(data);
+  } catch (error: any) {
+    console.error("Fetch 錯誤:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
